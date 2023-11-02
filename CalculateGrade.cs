5617dc8a0e5199da36bfa7e595e4109826c9c3ad
Jarek <jaroslaw.pela@wp.pl>
2023-11-02 23:28:30 +0100

    first commit

---
+++ CalculateGrade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculate
+{
+    public class CalculateGrade
+    {
+
+        public static string GradeCalculate (double percentage)
+        {
+            if (0 > percentage && percentage < 10)
+            {
+                return "a";
+            }
+            else if (0 > percentage && percentage < 10)
+            { 
+                return "b"; 
+            }
+            else if (0 > percentage && percentage < 10)
+            { 
+                return "c";
+            }
+            else if (0 > percentage && percentage < 10)
+            { 
+                return "d";
+            }
+            else if (0 > percentage && percentage < 10)
+            { 
+                return "e";
+            }
+      
+        }
+
+    }
+}
