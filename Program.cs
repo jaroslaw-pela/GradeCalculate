e396bef0b60c015a372f4b1a2afbec9f19058e54
Jarek <jaroslaw.pela@wp.pl>
2023-11-02 23:12:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GradeCalculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
